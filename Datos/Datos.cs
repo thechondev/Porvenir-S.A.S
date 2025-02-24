8d463ad42a07983a53458db3f3be69cfbbe511b5
thechondev <carlosandreslon7@gmail.com>
2025-02-24 10:36:37 -0500

    Agregar archivos de proyecto.

---
+++ Datos/Datos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Datos
+    {
+        public string codigo {  get; set; }
+        public string tipo { get; set; }
+        public string valor {  get; set; }
+        public string porcentaje_in {  get; set; }
+        public string valor_be {  get; set; }
+    }
+}
