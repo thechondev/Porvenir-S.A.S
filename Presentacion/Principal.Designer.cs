5f48218c75d6cccffdd26339e78a7ba54f9f28c3
thechondev <carlosandreslon7@gmail.com>
2025-02-25 22:28:11 -0500

    correciones menores

--- Presentacion/Principal.Designer.cs
+++ Presentacion/Principal.Designer.cs
@@ -28,20 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cmdguardar = new System.Windows.Forms.Button();
+            this.txtsegurocod = new System.Windows.Forms.TextBox();
+            this.txtsegurotip = new System.Windows.Forms.TextBox();
+            this.txtseguroval = new System.Windows.Forms.TextBox();
+            this.txtseguropor = new System.Windows.Forms.TextBox();
+            this.txtseguroval_b = new System.Windows.Forms.TextBox();
+            this.cboseguros = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
+            // 
+            // cmdguardar
+            // 
+            this.cmdguardar.Location = new System.Drawing.Point(61, 228);
+            this.cmdguardar.Name = "cmdguardar";
+            this.cmdguardar.Size = new System.Drawing.Size(100, 46);
+            this.cmdguardar.TabIndex = 0;
+            this.cmdguardar.Text = "Guardar seguro";
+            this.cmdguardar.UseVisualStyleBackColor = true;
+            this.cmdguardar.Click += new System.EventHandler(this.cmdguardar_Click);
+            // 
+            // txtsegurocod
+            // 
+            this.txtsegurocod.Location = new System.Drawing.Point(61, 79);
+            this.txtsegurocod.Name = "txtsegurocod";
+            this.txtsegurocod.Size = new System.Drawing.Size(100, 20);
+            this.txtsegurocod.TabIndex = 1;
+            // 
+            // txtsegurotip
+            // 
+            this.txtsegurotip.Location = new System.Drawing.Point(61, 105);
+            this.txtsegurotip.Name = "txtsegurotip";
+            this.txtsegurotip.Size = new System.Drawing.Size(100, 20);
+            this.txtsegurotip.TabIndex = 2;
+            // 
+            // txtseguroval
+            // 
+            this.txtseguroval.Location = new System.Drawing.Point(61, 131);
+            this.txtseguroval.Name = "txtseguroval";
+            this.txtseguroval.Size = new System.Drawing.Size(100, 20);
+            this.txtseguroval.TabIndex = 3;
+            // 
+            // txtseguropor
+            // 
+            this.txtseguropor.Location = new System.Drawing.Point(61, 157);
+            this.txtseguropor.Name = "txtseguropor";
+            this.txtseguropor.Size = new System.Drawing.Size(100, 20);
+            this.txtseguropor.TabIndex = 4;
+            // 
+            // txtseguroval_b
+            // 
+            this.txtseguroval_b.Location = new System.Drawing.Point(61, 183);
+            this.txtseguroval_b.Name = "txtseguroval_b";
+            this.txtseguroval_b.Size = new System.Drawing.Size(100, 20);
+            this.txtseguroval_b.TabIndex = 5;
+            // 
+            // cboseguros
+            // 
+            this.cboseguros.FormattingEnabled = true;
+            this.cboseguros.Location = new System.Drawing.Point(399, 78);
+            this.cboseguros.Name = "cboseguros";
+            this.cboseguros.Size = new System.Drawing.Size(121, 21);
+            this.cboseguros.TabIndex = 6;
             // 
             // Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cboseguros);
+            this.Controls.Add(this.txtseguroval_b);
+            this.Controls.Add(this.txtseguropor);
+            this.Controls.Add(this.txtseguroval);
+            this.Controls.Add(this.txtsegurotip);
+            this.Controls.Add(this.txtsegurocod);
+            this.Controls.Add(this.cmdguardar);
             this.Name = "Principal";
             this.Text = "Principal";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button cmdguardar;
+        private System.Windows.Forms.TextBox txtsegurocod;
+        private System.Windows.Forms.TextBox txtsegurotip;
+        private System.Windows.Forms.TextBox txtseguroval;
+        private System.Windows.Forms.TextBox txtseguropor;
+        private System.Windows.Forms.TextBox txtseguroval_b;
+        private System.Windows.Forms.ComboBox cboseguros;
     }
 }
 
--- Presentacion/Principal.cs
+++ Presentacion/Principal.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Negocio;
 
 namespace Presentacion
 {
@@ -27,6 +28,30 @@ namespace Presentacion
         public Principal()
         {
             InitializeComponent();
+            cargar_seguros();
+        }
+
+        CN_Negocio objetoCN = new CN_Negocio();
+        string[] seguro = new string[5];
+        public void cargar_seguros()
+        {
+            foreach (var lista in objetoCN.listado_seguros())
+            {
+                seguro = lista.Split(new char[] {','});
+                cboseguros.Items.Add((seguro[0] + " " + seguro[1]));
+            }
+        }
+
+        private void cmdguardar_Click(object sender, EventArgs e)
+        {
+            //invocar la funcion de insertar
+            objetoCN.Insertar_Seguros(txtsegurocod.Text,txtsegurotip.Text,txtseguroval.Text,txtseguropor.Text,txtseguroval_b.Text);
+
+            MessageBox.Show("Se ha registrado correctamente el producto", "Almacenes Don chucho", MessageBoxButtons.OK);
+
+            cargar_seguros();
         }
     }
+
+
 }
